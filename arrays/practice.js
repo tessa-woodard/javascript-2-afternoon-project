a3c634fb257064a1481a8cf77a4463e7aab455e8
Tessa Woodard <tessawoodard@outlook.com>
2020-08-06 23:16:40 -0400

    done

--- arrays/practice.js
+++ arrays/practice.js
@@ -17,8 +17,10 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
-
+function first(arr){
+    return arr[0]
+}
+first(arr)
 
 
 ////////// PROBLEM 2 //////////
@@ -32,7 +34,10 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+    return arr[2]
+}
+last(arr)
 
 
 
@@ -47,7 +52,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){
+  for (i = 0; i< family.length; i++){
+      alert (family[i])
+  }
+  console.log(family)
+}
+looper(family)
 
 
 
@@ -62,7 +73,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters){
+  for (i = letters.length - 1; i >=0; i--){
+    alert(letters[i])
+    console.log(letters[i])
+  }
+}
+reversedLooper(letters)
 
 
 
@@ -77,7 +94,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  let evenNumber= []
+
+  for (i = 0; i <= nums.length; i++){
+    if (nums[i] % 2 === 0){
+      evenNumber.push(nums[i])
+    }
+  }
+return evenNumber
+}
 
 
 
--- objects/practice.js
+++ objects/practice.js
@@ -14,7 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: 'Tessa',
+  age: 21
+}
+alert(me.name)
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +27,31 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: 'Fall Out Boy',
+  food: 'pizza',
+  person: 'Terry',
+  book: 'Divergent',
+  movie: 'Up',
+  holiday: 'Halloween'
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings['car'] = 'Hyundai'
+favoriteThings['brand'] = 'Elantra'
+
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings['food'] = 'Chicken Nuggets'
+favoriteThings['book'] = 'Harry Potter'
 
 ////////// PROBLEM 3 //////////
 
@@ -49,13 +63,19 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
+let backPack = {}
+
+let item= 'firstPocket'
+
+backPack[item] = 'chapstick'
+backPack.color = 'black'
+
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert(backPack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +83,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -84,7 +104,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
